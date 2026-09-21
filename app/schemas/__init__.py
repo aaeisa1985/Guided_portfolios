08f66220bf19956ed582abf11e483e89117fe1ad
aaeisa1985 <a.a.eisa.1985@gmail.com>
2026-09-21 13:52:34 +0400

    refactor: expose domain schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from .auth import RegisterIn,LoginIn,TokenOut
+from .portfolio import PortfolioOut
+from .suitability import SuitabilityIn
+from .simulator import SimulatorIn
+from .subscription import SubscriptionIn
+from .consent import ConsentIn
+from .investment import InstrumentIn,LedgerEntryIn,OrderIn
