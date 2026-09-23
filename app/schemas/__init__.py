73bb4694e014e12062f974f43f27e80afe6632e0
aaeisa1985 <a.a.eisa.1985@gmail.com>
2026-09-23 09:48:24 +0400

    feat: export manager version schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -9,3 +9,4 @@ from .investment import InstrumentIn,LedgerEntryIn,OrderIn,ExecutionIn
 from .admin import AdminLoginIn,PortfolioCreateIn,PortfolioUpdateIn,AdminUserUpdateIn,AdminPortfolioResponse
 
 from .manager import ManagerPortfolioCreateIn,ManagerPortfolioUpdateIn,ManagerCompositionIn,ManagerInstrumentCreateIn,ManagerDocumentCreateIn,ManagerPerformanceIn
+,ManagerVersionIn
