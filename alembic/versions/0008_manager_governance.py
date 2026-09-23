ac7450eb6077cb4402375aa39169b5615b360e49
aaeisa1985 <a.a.eisa.1985@gmail.com>
2026-09-23 09:49:10 +0400

    chore: record manager governance schema migration

---
+++ alembic/versions/0008_manager_governance.py
@@ -0,0 +1,13 @@
+"""manager portfolio governance extensions"""
+
+revision = "0008"
+down_revision = "0007"
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
