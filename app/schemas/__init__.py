eab38b8f1702d47d2585a2ea7025194247613c25
aaeisa1985 <a.a.eisa.1985@gmail.com>
2026-09-23 09:24:49 +0400

    feat: export manager schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -7,3 +7,5 @@ from .consent import ConsentIn
 from .investment import InstrumentIn,LedgerEntryIn,OrderIn,ExecutionIn
 
 from .admin import AdminLoginIn,PortfolioCreateIn,PortfolioUpdateIn,AdminUserUpdateIn,AdminPortfolioResponse
+
+from .manager import ManagerPortfolioCreateIn,ManagerPortfolioUpdateIn,ManagerCompositionIn,ManagerInstrumentCreateIn,ManagerDocumentCreateIn,ManagerPerformanceIn
